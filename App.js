48acdfbce9c8034b87777cad5a654e0cbe226e57
Thomas Draper <tom@betterservers.com>
2017-06-18 23:25:19 -0600

    Working before pulling new dictionary style

--- App.js
+++ App.js
@@ -69,6 +69,11 @@ function WordInfo() {
   });
 }
 
+function WordInfo2() {
+  return [{'timestamp': 1497712078.1578422, 'cardbox': 1, 'dict': 'OWL14', 'name': 'SYN', 'isWord': 1}, {'timestamp': 1497712081.3843472, 'cardbox': 1, 'dict': 'OWL14', 'name': 'REA', 'isWord': 0}, {'timestamp': 1497712083.3533807, 'cardbox': 2, 'dict': 'OWL14', 'name': 'AJE', 'isWord': 0}, {'timestamp': 1497712084.2478843, 'cardbox': 2, 'dict': 'OWL14', 'name': 'VAI', 'isWord': 0}, {'timestamp': 1497712088.6850865, 'cardbox': 2, 'dict': 'OWL14', 'name': 'VIT', 'isWord': 0}, {'timestamp': 1497712090.5375645, 'cardbox': 2, 'dict': 'OWL14', 'name': 'CAM', 'isWord': 1}, {'timestamp': 1497712091.5030003, 'cardbox': 2, 'dict': 'OWL14', 'name': 'KIT', 'isWord': 1}, {'timestamp': 1497712092.168156, 'cardbox': 7, 'dict': 'OWL14', 'name': 'TWO', 'isWord': 1}, {'timestamp': 1497712093.2779875, 'cardbox': 8, 'dict': 'OWL14', 'name': 'SAA', 'isWord': 0}, {'timestamp': 1497712094.9799373, 'cardbox': 8, 'dict': 'OWL14', 'name': 'RHY', 'isWord': 0}];
+}
+
+
 export default class WordQuiz extends React.Component {
   constructor() {
     super();
@@ -83,6 +88,8 @@ export default class WordQuiz extends React.Component {
  
   handlePress(bool) {
     const word = this.state.quizOrder[this.state.pos];
+    const milliseconds = (new Date).getTime();
+    console.log(milliseconds);
     if (this.state.wordValid[word] == bool) {
       this.setState({
         pos: (this.state.pos + 1) % this.state.quizOrder.length,
@@ -98,15 +105,31 @@ export default class WordQuiz extends React.Component {
 
   async fetchData() {
     try {
-      let response = await fetch('http://192.168.1.50:9000/json');
-      let responsJSON = await response.json();
+      let response = await fetch('http://50.114.241.53/json');
+      let responseJSON = await response.json();
+      console.log(responseJSON);
+      this.setState({
+        wordValid: responseJSON.wordValid,
+        quizOrder: responseJSON.quizOrder,
+      });
     } catch(error) {
       console.error(error);
     }
-    this.setState({
-      wordvalid: responseJSON.wordValid,
-      quizOrder: responseJSON.quizOrder,
-    });
+  }
+
+  async fetchData2() {
+    try {
+      let response = await fetch('http://50.114.241.53/quiz');
+      let responseJSON = await response.json();
+      console.log(responseJSON);
+      this.setState({
+        quiz: response.JSON,
+        wordValid: responseJSON.wordValid,
+        quizOrder: responseJSON.quizOrder,
+      });
+    } catch(error) {
+      console.error(error);
+    }
   }
 
   componentDidMount() {
