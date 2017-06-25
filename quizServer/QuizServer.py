ebe8bf7640fb0e5e71ee962cd86e1526553b3b0d
Thomas Draper <tom@betterservers.com>
2017-06-25 00:13:32 -0600

    Successful delivery of sql response via JSON

--- quizServer/QuizServer.py
+++ quizServer/QuizServer.py
@@ -1,49 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import asyncio
+from gevent import monkey; monkey.patch_all()
+import gevent
+
 import bottle
 import contextlib
 import itertools
 import sqlite3
 import sys
-import termios
 import time
 import tty
 
 
-@bottle.route('/quiz')
-def returnQuiz(limit=100, filename='quiz.sqlite3'):
-    conn = sqlite3.connect(filename)
-    conn.row_factory = sqlite3.Row  # Allow dict access for each row
-    cur = conn.cursor()
-    cur.execute('''
-        SELECT name, dict, isWord, cardbox, MAX(timestamp)
-        AS name, dict, isWord, cardbox, timestamp
-        FROM questions
-        GROUP BY name
-        ORDER BY MAX(timestamp) +  65536 * cardbox*cardbox
-        LIMIT ''' + '%d;' % limit)
-    print([dict(item) for item in cur])
-    conn.close()
+class quizServer(bottle.Bottle):
+    def __init__(self, sqlfile, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.conn = sqlite3.connect(sqlfile)
+        self.conn.row_factory = sqlite3.Row  # Allow dict access for each row
+        self.get('/quiz', callback=self.returnQuiz)
 
+    def __del__(self):
+        self.conn.close()
 
-
-def getQuestions(conn: 'SQLconnection', limit):
-    # find latest timestamp entry for each word
-    # order by timestamp + function(cardbox) seconds
-    cur = conn.cursor()
-    cur.execute('''
-        SELECT name, dict, isWord, cardbox, MAX(timestamp) FROM questions
-        GROUP BY name
-        ORDER BY MAX(timestamp) +  65536 * cardbox*cardbox
-        LIMIT ''' + '%d;' % limit)
-    yield from cur
-
+    def returnQuiz(self, limit=1000, filename='quiz.sqlite3'):
+        cur = self.conn.cursor()
+        cur.execute('''
+            SELECT name, dict, isWord, cardbox, MAX(timestamp) AS timestamp
+            FROM questions
+            GROUP BY name
+            ORDER BY timestamp +  65536 * cardbox*cardbox
+            LIMIT ''' + '%d;' % limit)
+        return {'quiz': [dict(item) for item in cur]}
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         limit = int(sys.argv[1])
     else:
-        limit = 100
-    print(returnQuiz(limit))
+        limit = 10
+    quiz = quizServer('quiz.sqlite3')
+    quiz.run(host='0.0.0.0', port=80, server='gevent')
